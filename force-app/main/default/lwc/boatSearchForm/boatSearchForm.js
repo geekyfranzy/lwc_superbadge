39e19431c1a703626e5e89f1b9112ad46f89f0a9
Htoo Franzy <htoofranz100@gmail.com>
2022-05-16 16:26:33 +0800

    Challenge 16

--- force-app/main/default/lwc/boatSearchForm/boatSearchForm.js
+++ force-app/main/default/lwc/boatSearchForm/boatSearchForm.js
@@ -1,36 +1,41 @@
-// imports
+import { LightningElement, track, wire } from 'lwc';
 import getBoatTypes from '@salesforce/apex/BoatDataService.getBoatTypes';
-import { LightningElement, wire } from 'lwc';
+
 export default class BoatSearchForm extends LightningElement {
+    
     selectedBoatTypeId = '';
     
     // Private
     error = undefined;
     
+    // Needs explicit track due to nested data
+    @track
     searchOptions;
     
     // Wire a custom Apex method
     @wire(getBoatTypes)
-    boatTypes({ error, data }) {
-      if (data) {
-        this.searchOptions = data.map(type => {
-          // TODO: complete the logic
-          return {label: type.Name, value: type.Id}
-        });
-        this.searchOptions.unshift({ label: 'All Types', value: '' });
-      } else if (error) {
-        this.searchOptions = undefined;
-        this.error = error;
-      }
+    boatTypes({ data, error }) {
+        if (data) {
+            this.searchOptions = data.map(type => {
+                return { label: type.Name, value: type.Id };
+            });
+            this.searchOptions.unshift({ label: 'All Types', value: '' });
+        } else if (error) {
+            this.searchOptions = undefined;
+            this.error = error;
+        }
     }
     
     // Fires event that the search option has changed.
     // passes boatTypeId (value of this.selectedBoatTypeId) in the detail
     handleSearchOptionChange(event) {
-      // Create the const searchEvent
-      this.selectedBoatTypeId=event.detail.value;
-      // searchEvent must be the new custom event search
-      const searchEvent= new CustomEvent('search',{detail:{boatTypeId:this.selectedBoatTypeId}});
-      this.dispatchEvent(searchEvent);
+        this.selectedBoatTypeId = event.detail.value
+        // Create the const searchEvent        
+        const searchEvent = new CustomEvent('search', { 
+            detail: {
+                boatTypeId: this.selectedBoatTypeId
+            }
+        });
+        this.dispatchEvent(searchEvent);
     }
-  }
+}  
